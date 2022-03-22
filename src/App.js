abb1286674cb6622ef291947ac52d327c8f95a3f
carlos <bobcn_1991@hotmail.com>
2022-03-22 00:07:32 +0000

    init apollo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  useQuery,
+  gql
+} from "@apollo/client";
 
+export const GET_ARTBOARDS = gql`
+query getArtboards($documentId: ID!) {
+  share(id: $documentId) {
+    identifier
+    version {
+      document {
+        name
+        artboards {
+          entries {
+            name
+            isArtboard
+            files {
+              url
+              height
+              width
+              scale
+              thumbnails {
+                url
+                height
+                width
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`;
 function App() {
+
+  const { loading, error, data } = useQuery(GET_ARTBOARDS, {
+    variables: { documentId: "e981971c-ff57-46dc-a932-a60dc1804992" },
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error){
+      console.log(error)
+     return <p>Error :</p>
+    };
+
+   console.log(data)
+
   return (
     <div className="App">
       <header className="App-header">
--- src/index.js
+++ src/index.js
@@ -4,10 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: 'https://graphql.sketch.cloud/api',
+  cache: new InMemoryCache()
+});
+
 ReactDOM.render(
+  <ApolloProvider client={client}>
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+   
+      <App />
+    
+  </React.StrictMode>
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
